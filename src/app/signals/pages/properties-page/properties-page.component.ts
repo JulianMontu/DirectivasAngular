ce1ad7a5b0f8bdb598a14e97491eecb339f0361e
JulianMontu <julamontufar@misena.edu.co>
2024-08-10 15:23:02 -0500

    Señales con efectos

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,11 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, effect, OnDestroy, OnInit, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
-export class PropertiesPageComponent {
+export class PropertiesPageComponent implements OnDestroy, OnInit {
+ 
+  public counter = signal(10);
 
   public user = signal<User>({
     id: 1,
@@ -14,6 +16,29 @@ export class PropertiesPageComponent {
     last_name: 'Bluth',
     avatar: 'https://reqres.in/img/faces/1-image.jpg'
   });
+
+  public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`);
+
+  public userChangedEffect = effect( () => {
+    // Todo: Descomentar esta línea
+    console.log( `${ this.user().first_name } - ${ this.counter() } ` );
+  } );
+
+  ngOnInit(): void {
+    // setInterval(()=> {
+    //   this.counter.update(current => current +1)
+
+    //   if(this.counter() == 15)
+    //     this.userChangedEffect.destroy();
+    // },1000)
+  }
+  ngOnDestroy(): void {
+    this.userChangedEffect.destroy();
+  }
+
+  increasyBy(value: number){
+    this.counter.update(current => current + value);
+  }
 
   onFieldUpdate(field: keyof User, value: string){
 
