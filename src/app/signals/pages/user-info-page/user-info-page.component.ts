481b4720f12856ae5086f924d16af116a8c01e13
JulianMontu <julamontufar@misena.edu.co>
2024-08-10 14:33:55 -0500

    Señales como objetos y manejador de errores

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { UserService } from '../../services/user-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+  private usersService = inject(UserService);
+
+  public userId = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+  public fullName = computed<string>( () =>{
+    if(!this.currentUser()) return 'Usuario no encontrado';
+
+    return `${this.currentUser()?.first_name} ${this.currentUser()?.last_name}`;
+  });
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number){
+    if(id <= 0 ) return;
+
+    this.userId.set(id);
+
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById(id)
+    .subscribe({
+      next: (value) => {
+        this.currentUser.set(value);
+        this.userWasFound.set(true);
+      },
+      error: () => {
+        this.userWasFound.set(false)
+        this.currentUser.set(undefined);
+      },
+    });
+  }
 
 }
